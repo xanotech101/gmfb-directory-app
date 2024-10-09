2fa3d1933a5d33b1896488c3d49175f5265615b9
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-10-09 14:32:03 +0100

    fix: used tabs component instead of state management

--- src/app/(root)/settings/page.tsx
+++ src/app/(root)/settings/page.tsx
@@ -1,231 +1,147 @@
-'use client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Label } from '@radix-ui/react-Label'
-import { useState } from 'react'
+import { Label } from '@/components/ui/label'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export default function Settings() {
-  const [activeSection, setActiveSection] = useState<'account' | 'password'>('account')
-
-  const [profile, setProfile] = useState({
-    username: 'JohnDoe',
-    phone: '123-456-7890',
-    email: 'johndoe@example.com',
-    created_at: '2020-05-01',
-    gender: 'Male',
-    birth_date: '1990-01-01',
-    age: 34,
-  })
-
-  const [passwords, setPasswords] = useState({
-    currentPassword: '',
-    newPassword: '',
-    confirmPassword: '',
-  })
-
-  const handleProfileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setProfile({ ...profile, [e.target.name]: e.target.value })
-  }
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPasswords({ ...passwords, [e.target.name]: e.target.value })
-  }
-
-  const handleProfileSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    console.log('Profile Updated', profile)
-  }
-
-  const handlePasswordSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    console.log('Password Updated', passwords)
-  }
-
   return (
-    <div className="mx-auto">
-      <div className="flex space-x-4 mb-8">
-        <button
-          onClick={() => setActiveSection('account')}
-          className={`px-4 py-2 rounded ${
-            activeSection === 'account' ? 'bg-[#891C69] text-white' : 'bg-gray-200'
-          }`}
-        >
-          Your Account Information
-        </button>
-        <button
-          onClick={() => setActiveSection('password')}
-          className={`px-4 py-2 rounded ${
-            activeSection === 'password' ? 'bg-[#891C69] text-white' : 'bg-gray-200'
-          }`}
-        >
-          Change Your Password
-        </button>
-      </div>
-
-      {activeSection === 'account' && (
-        <div>
-          <h2 className="text-xl font-semibold mb-4">Account Information</h2>
-          <form onSubmit={handleProfileSubmit} className="space-y-4">
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="username">
-                Username
-              </Label>
+    <Tabs defaultValue="account" className="w-full">
+      <TabsList className="grid w-full grid-cols-2 h-[45px]">
+        <TabsTrigger className="h-[35px]" value="account">
+          Account
+        </TabsTrigger>
+        <TabsTrigger className="h-[35px]" value="password">
+          Password
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when {"you're"} done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
               <Input
-                type="text"
-                name="username"
+                className="focus:outline-none focus:border-[#891C69]"
+                id="name"
+                defaultValue="Pedro Duarte"
+              />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input
+                className="focus:outline-none focus:border-[#891C69]"
                 id="username"
-                value={profile.username}
-                onChange={handleProfileChange}
-                className="w-full p-2 border-b rounded"
+                defaultValue="@peduarte"
               />
             </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="phone">
-                Phone
-              </Label>
+            <div className="space-y-1">
+              <Label htmlFor="phone">Phone</Label>
               <Input
-                type="text"
-                name="phone"
-                id="phone"
-                value={profile.phone}
-                onChange={handleProfileChange}
-                className="w-full p-2 border-b rounded"
+                className="focus:outline-none focus:border-[#891C69]"
+                id="username"
+                defaultValue="08101194855"
               />
             </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="email">
-                Email
-              </Label>
+            <div className="space-y-1">
+              <Label htmlFor="email">Email</Label>
               <Input
-                type="email"
-                name="email"
+                className="focus:outline-none focus:border-[#891C69]"
                 id="email"
-                value={profile.email}
-                onChange={handleProfileChange}
-                className="w-full p-2 border-b rounded"
+                defaultValue="peduarte@gmail.com"
               />
             </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="created_at">
-                Account Creation
-              </Label>
+            <div className="space-y-1">
+              <Label htmlFor="account_creation">Account Creation</Label>
               <Input
-                type="text"
-                name="created_at"
-                id="created_at"
-                value={profile.created_at}
+                className="focus:outline-none focus:border-[#891C69]"
+                id="account_creation"
                 readOnly
-                className="w-full p-2 border-b rounded bg-gray-100"
+                defaultValue="Oct 27, 2019, 9:54:41 PM"
               />
             </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="gender">
-                Gender
-              </Label>
+            <div className="space-y-1">
+              <Label htmlFor="account_creation">Gender</Label>
               <Input
-                type="text"
-                name="gender"
+                className="focus:outline-none focus:border-[#891C69]"
                 id="gender"
-                value={profile.gender}
-                onChange={handleProfileChange}
-                className="w-full p-2 border-b rounded"
+                defaultValue="Male"
               />
             </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="birth_date">
-                Birth Date
-              </Label>
+            <div className="space-y-1">
+              <Label htmlFor="birth_date">Birth Date</Label>
               <Input
-                type="date"
-                name="birth_date"
+                className="focus:outline-none focus:border-[#891C69]"
                 id="birth_date"
-                value={profile.birth_date}
-                onChange={handleProfileChange}
-                className="w-full p-2 border-b rounded"
-              />
-            </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="age">
-                Age
-              </Label>
-              <Input
-                type="number"
-                name="age"
-                id="age"
-                value={profile.age}
                 readOnly
-                className="w-full p-2 border-b rounded bg-gray-100"
+                defaultValue="Oct 27, 2019"
               />
             </div>
-
-            <Button type="submit" className="bg-[#891C69] hover:bg-[#974D7B]">
-              Save Changes
-            </Button>
-          </form>
-        </div>
-      )}
-
-      {activeSection === 'password' && (
-        <div>
-          <h2 className="text-xl font-semibold mb-4">Change Password</h2>
-          <form onSubmit={handlePasswordSubmit} className="space-y-4">
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="currentPassword">
-                Current Password
-              </Label>
+            <div className="space-y-1">
+              <Label htmlFor="account_creation">Age</Label>
               <Input
-                type="password"
-                name="currentPassword"
-                id="currentPassword"
-                value={passwords.currentPassword}
-                onChange={handlePasswordChange}
-                className="w-full p-2 border-b rounded"
-                required
+                className="focus:outline-none focus:border-[#891C69]"
+                id="age"
+                defaultValue="34"
               />
             </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="newPassword">
-                New Password
-              </Label>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, {"you'll"} be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
               <Input
+                className="focus:outline-none focus:border-[#891C69]"
+                id="current"
                 type="password"
-                name="newPassword"
-                id="newPassword"
-                value={passwords.newPassword}
-                onChange={handlePasswordChange}
-                className="w-full p-2 border-b rounded"
-                required
               />
             </div>
-
-            <div>
-              <Label className="block text-sm font-medium mb-1" htmlFor="confirmPassword">
-                Confirm New Password
-              </Label>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
               <Input
+                className="focus:outline-none focus:border-[#891C69]"
+                id="new"
                 type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-                value={passwords.confirmPassword}
-                onChange={handlePasswordChange}
-                className="w-full p-2 border-b rounded"
-                required
               />
             </div>
-
-            <Button type="submit" className="bg-[#891C69] hover:bg-[#974D7B]">
-              Change Password
-            </Button>
-          </form>
-        </div>
-      )}
-    </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">Confirm password</Label>
+              <Input
+                className="focus:outline-none focus:border-[#891C69]"
+                id="confirm"
+                type="password"
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
   )
 }
