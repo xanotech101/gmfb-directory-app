3d6231b5b6a71627101957baffa4e0f9a41c1620
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2024-11-27 13:29:41 +0100

    chore: fixes

--- src/app/(root)/announcements/create/page.tsx
+++ src/app/(root)/announcements/create/page.tsx
@@ -32,11 +32,11 @@ const formSchema = z.object({
   departments: z.array(z.object({
     label: z.string(),
     value: z.string(),
-  })).min(1).optional().default([]),
+  })).optional().default([]),
   users: z.array(z.object({
     label: z.string(),
     value: z.string(),
-  })).min(1).optional().default([]),
+  })).optional().default([]),
 })
 
 export default function CreateAnnouncement() {
