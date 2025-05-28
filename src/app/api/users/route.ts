d90cf158e1cb44575af8aa87cb499882bdaadd8e
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-05-28 13:47:28 +0100

    chore: fix pagination on users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,20 +3,21 @@ import { get, post } from '@/lib/fetch'
 import { NextResponse } from 'next/server'
 import { getTokens } from '@/lib/get-tokens'
 import { handleServerError } from '@/lib/handle-server-error'
+import { ITEMS_PER_PAGE } from '@/constants'
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const page = searchParams.get('page') || '1'
-  const limit = searchParams.get('limit') || '50'
+  const limit = searchParams.get('limit') || ITEMS_PER_PAGE
   const search = searchParams.get('search') || ''
 
-  const {accessToken} = await getTokens()
+  const { accessToken } = await getTokens()
   const response = await get<any>(`/api/v1/users?page=${page}&limit=${limit}&search=${search}`, {
     options: {
       headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    }
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
   })
 
   return NextResponse.json(response)
@@ -30,12 +31,12 @@ export async function POST(request: Request) {
       body,
       options: {
         headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      }
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
     })
     return NextResponse.json(response)
-  }catch(error) {
+  } catch (error) {
     return handleServerError(error)
   }
 }
