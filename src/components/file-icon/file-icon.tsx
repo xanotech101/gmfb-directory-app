f681a53b045a8b432f82a30f6c8028f171ce4254
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-06-03 10:42:57 +0100

    chore: fixes

--- src/components/file-icon/file-icon.tsx
+++ src/components/file-icon/file-icon.tsx
@@ -10,21 +10,21 @@ interface FileIconProps extends Partial<ComponentProps<typeof Image>> {
 export const FileIcon = ({ type, imageUrl, ...props }: FileIconProps) => {
   const getFileIcon = () => {
     if (type.startsWith('image/')) {
-      return <FileImage className="h-12 w-12 text-blue-500" />
+      return <FileImage className="size-10 text-blue-500 flex-shrink-0" />
     } else if (type.startsWith('video/')) {
-      return <FileVideo className="h-12 w-12 text-purple-500" />
+      return <FileVideo className="size-10 text-purple-500 flex-shrink-0" />
     } else if (type.includes('text') || type.includes('document')) {
-      return <FileText className="h-12 w-12 text-green-500" />
+      return <FileText className="size-10 text-green-500 flex-shrink-0" />
     } else {
-      return <File className="h-12 w-12 text-gray-500" />
+      return <File className="size-10 text-gray-500 flex-shrink-0" />
     }
   }
 
   return type.startsWith('image/') ? (
     <Image
-      className="w-12 h-12 object-cover rounded-lg"
-      width={48}
-      height={48}
+      className="size-10 object-cover rounded-lg flex-shrink-0"
+      width={40}
+      height={40}
       {...props}
       alt={props.alt ?? 'File Icon'}
       src={imageUrl}
