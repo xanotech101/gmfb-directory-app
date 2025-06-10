483a0e5d6434ff461e36d0ea00f7cddd4fa8a798
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-06-10 16:19:05 +0100

    chore: update topbar styles

--- src/app/(dashboard)/_components/layout/topbar/topbar.tsx
+++ src/app/(dashboard)/_components/layout/topbar/topbar.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import {
@@ -15,6 +14,16 @@ import { post } from '@/lib/fetch'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { UserAvatar } from '@/components/user-avatar/user-avatar'
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+  MenubarTrigger,
+  MenubarMenu,
+} from '@/components/ui/menubar'
+import { Badge } from '@/components/ui/badge'
+import { Power, UserCircle } from 'lucide-react'
 
 const userNavigation = [{ name: 'Your profile', href: 'settings' }]
 
@@ -70,51 +79,59 @@ export const Topbar = ({ setSidebarOpen }: { setSidebarOpen(open: boolean): void
               <div aria-hidden="true" className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-200" />
 
               {/* Profile dropdown */}
-              <Menu as="div" className="relative">
-                <MenuButton className="-m-1.5 flex items-center p-1.5">
-                  <span className="sr-only">Open user menu</span>
-                  <UserAvatar firstName={user?.first_name} lastName={user?.last_name} />
-                  <span className="hidden lg:flex lg:items-center">
-                    <span
-                      aria-hidden="true"
-                      className="ml-1.5 text-sm font-semibold leading-6 text-gray-900"
-                    >
-                      {user?.first_name} {user?.last_name}
-                    </span>
-                    <ChevronDownIcon
-                      aria-hidden="true"
-                      className="ml-2 h-5 w-5 text-gray-400 hover:text-[#891C69]"
-                    />
-                  </span>
-                </MenuButton>
-                <MenuItems
-                  transition
-                  className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                >
-                  {userNavigation.map((item) => (
-                    <MenuItem key={item.name}>
-                      <Link
-                        href={item.href}
-                        className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-50"
+              <Menubar className="relative">
+                <MenubarMenu>
+                  <MenubarTrigger className="-m-1.5 flex items-center p-1.5 border-transparent outline-0 outline !bg-white cursor-pointer">
+                    <span className="sr-only">Open user menu</span>
+                    <UserAvatar firstName={user?.first_name} lastName={user?.last_name} />
+                    <span className="hidden lg:flex lg:items-center">
+                      <span
+                        aria-hidden="true"
+                        className="ml-1.5 text-sm font-semibold leading-6 text-gray-900"
                       >
-                        {item.name}
+                        {user?.first_name} {user?.last_name}
+                      </span>
+                      <ChevronDownIcon
+                        aria-hidden="true"
+                        className="ml-2 size-5 text-gray-400 hover:text-[#891C69]"
+                      />
+                    </span>
+                  </MenubarTrigger>
+                  <MenubarContent>
+                    <MenubarItem className="flex flex-col items-start">
+                      <p>
+                        {user?.first_name} {user?.last_name}
+                      </p>
+                      <p className="text-gray-600 truncate max-w-52">{user?.email}</p>
+                      <Badge className="mt-3" variant="green">
+                        {user?.role?.name}
+                      </Badge>
+                    </MenubarItem>
+                    <MenubarSeparator />
+
+                    <MenubarItem>
+                      <Link href="settings" className="flex items-center gap-2">
+                        <UserCircle className="size-4" />
+                        Your Profile
                       </Link>
-                    </MenuItem>
-                  ))}
-                  <MenuItem>
-                    <button
-                      onClick={() =>
-                        post('/api/auth/logout', { isClient: true }).finally(() =>
-                          router.push('/login'),
-                        )
-                      }
-                      className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-50 w-full text-left"
-                    >
-                      Logout
-                    </button>
-                  </MenuItem>
-                </MenuItems>
-              </Menu>
+                    </MenubarItem>
+
+                    <MenubarItem>
+                      <button
+                        onClick={() =>
+                          post('/api/auth/logout', { isClient: true }).finally(() =>
+                            router.push('/login'),
+                          )
+                        }
+                        className="flex items-center gap-2 w-full"
+                      >
+                        <Power className="size-4" />
+                        Logout
+                      </button>
+                    </MenubarItem>
+                  </MenubarContent>
+                </MenubarMenu>
+              </Menubar>
             </div>
           </div>
         </div>
